95914b288678d93f0927605b8b3ef3a7bca22c2f
Kolin Lu <kolinlu@google.com>
2024-03-13 21:30:33 -0700

    Update uiautomator lib to stable 2.3.0 (#45)

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import setuptools
 
 setuptools.setup(
     name='snippet-uiautomator',
-    version='1.0.5',
+    version='1.1.0',
     author='Kolin Lu',
     author_email='kolinlu@google.com',
     description='A Python wrapper for UiAutomator based on Mobly Snippet Lib.',
