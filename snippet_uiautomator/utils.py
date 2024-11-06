0085ec4259d282e339b2dae7a7be2fd6ea0e4518
Kolin Lu <kolinlu@google.com>
2024-11-06 13:04:03 -0800

    Handle non-positive wait time and fix waitUntilTrue doesn't run at least once

--- snippet_uiautomator/utils.py
+++ snippet_uiautomator/utils.py
@@ -33,9 +33,14 @@ TimeUnit = Union[float, int, datetime.timedelta]
 
 def covert_to_millisecond(timeout: TimeUnit) -> int:
   """Converts a time unit object to an integer in milliseconds."""
-  if isinstance(timeout, datetime.timedelta):
-    return int(timeout.total_seconds() * 1_000)
-  return int(timeout)
+  timeout_ms = (
+      int(timeout.total_seconds() * 1_000)
+      if isinstance(timeout, datetime.timedelta)
+      else int(timeout)
+  )
+  if timeout_ms <= 0:
+    raise ValueError('Timeout must be greater than 0 second')
+  return timeout_ms
 
 
 def get_latest_logcat_timestamp(ad: android_device.AndroidDevice) -> str:
